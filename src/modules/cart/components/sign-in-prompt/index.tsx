5d9599018deca43c26ad7c693167f854ce693440
MatthewOlaka <matthew.olaka0@gmail.com>
2025-11-24 18:33:52 -0800

    Added the cart page and mobile responsive too

--- src/modules/cart/components/sign-in-prompt/index.tsx
+++ src/modules/cart/components/sign-in-prompt/index.tsx
@@ -1,26 +1,29 @@
-import { Button, Heading, Text } from "@medusajs/ui"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import { Heading, Text } from '@medusajs/ui';
+import { Button } from '@modules/common/components/button';
+import LocalizedClientLink from '@modules/common/components/localized-client-link';
 
 const SignInPrompt = () => {
-  return (
-    <div className="bg-white flex items-center justify-between">
-      <div>
-        <Heading level="h2" className="txt-xlarge">
-          Already have an account?
-        </Heading>
-        <Text className="txt-medium text-ui-fg-subtle mt-2">
-          Sign in for a better experience.
-        </Text>
-      </div>
-      <div>
-        <LocalizedClientLink href="/account">
-          <Button variant="secondary" className="h-10" data-testid="sign-in-button">
-            Sign in
-          </Button>
-        </LocalizedClientLink>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="bg-yellow-100 flex items-center justify-between">
+			<div>
+				<Heading level="h2" className="text-xl md:text-3xl font-serif text-green-900 ">
+					Already have an account?
+				</Heading>
+				<Text className="text-sm md:text-md mt-1 text-slate-700">
+					Sign in for a better experience.
+				</Text>
+			</div>
+			<div>
+				<LocalizedClientLink href="/account">
+					<Button
+						wrapperClass="px-6 py-1.5 !text-green-900 font-semibold xs:mb-8 rounded-lg border border-amber-300"
+						primaryColor="bg-yellow-400 hover:bg-yellow-400/70"
+						text="Sign in"
+					/>
+				</LocalizedClientLink>
+			</div>
+		</div>
+	);
+};
 
-export default SignInPrompt
+export default SignInPrompt;
--- src/modules/cart/templates/summary.tsx
+++ src/modules/cart/templates/summary.tsx
@@ -1,48 +1,51 @@
-"use client"
+'use client';
 
-import { Button, Heading } from "@medusajs/ui"
+import { Heading } from '@medusajs/ui';
 
-import CartTotals from "@modules/common/components/cart-totals"
-import Divider from "@modules/common/components/divider"
-import DiscountCode from "@modules/checkout/components/discount-code"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import { HttpTypes } from "@medusajs/types"
+import CartTotals from '@modules/common/components/cart-totals';
+import Divider from '@modules/common/components/divider';
+import DiscountCode from '@modules/checkout/components/discount-code';
+import LocalizedClientLink from '@modules/common/components/localized-client-link';
+import { HttpTypes } from '@medusajs/types';
+import { Button } from '@modules/common/components/button';
 
 type SummaryProps = {
-  cart: HttpTypes.StoreCart & {
-    promotions: HttpTypes.StorePromotion[]
-  }
-}
+	cart: HttpTypes.StoreCart & {
+		promotions: HttpTypes.StorePromotion[];
+	};
+};
 
 function getCheckoutStep(cart: HttpTypes.StoreCart) {
-  if (!cart?.shipping_address?.address_1 || !cart.email) {
-    return "address"
-  } else if (cart?.shipping_methods?.length === 0) {
-    return "delivery"
-  } else {
-    return "payment"
-  }
+	if (!cart?.shipping_address?.address_1 || !cart.email) {
+		return 'address';
+	} else if (cart?.shipping_methods?.length === 0) {
+		return 'delivery';
+	} else {
+		return 'payment';
+	}
 }
 
 const Summary = ({ cart }: SummaryProps) => {
-  const step = getCheckoutStep(cart)
+	const step = getCheckoutStep(cart);
 
-  return (
-    <div className="flex flex-col gap-y-4">
-      <Heading level="h2" className="text-[2rem] leading-[2.75rem]">
-        Summary
-      </Heading>
-      <DiscountCode cart={cart} />
-      <Divider />
-      <CartTotals totals={cart} />
-      <LocalizedClientLink
-        href={"/checkout?step=" + step}
-        data-testid="checkout-button"
-      >
-        <Button className="w-full h-10">Go to checkout</Button>
-      </LocalizedClientLink>
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col gap-y-4">
+			<Heading level="h2" className="text-4xl font-serif text-green-900">
+				Summary
+			</Heading>
+			<DiscountCode cart={cart} />
+			<Divider />
+			<CartTotals totals={cart} />
+			<LocalizedClientLink href={'/checkout?step=' + step} data-testid="checkout-button">
+				{/* <Button className="w-full h-10">Go to checkout</Button> */}
+				<Button
+					wrapperClass="px-6 py-2 !text-green-900 font-semibold xs:mb-8 rounded-lg border border-amber-300 w-full"
+					primaryColor="bg-yellow-400 hover:bg-yellow-400/70"
+					text="Go to checkout"
+				/>
+			</LocalizedClientLink>
+		</div>
+	);
+};
 
-export default Summary
+export default Summary;
